c07eae6392b0437618120e3556209fd559193898
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2022-04-15 19:03:30 +0530

    firebase installed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
-import Caption from './components/Caption';
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <div className='form-and-banner-wrapper'>
-          <div className='form-block'>
-            <Form/>
-          </div>
-
-          <div className='banner-block'>
-            <Caption />
-          </div>
-        </div>
-      </div>
+      Pawan Pradhan
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyC_ZAGujQtlSSuDzhgdVV8lirKviGZNj2Y",
+   authDomain: "clone-22287.firebaseapp.com",
+   projectId: "clone-22287",
+   storageBucket: "clone-22287.appspot.com",
+   messagingSenderId: "110358648705",
+   appId: "1:110358648705:web:72332cd5a37f6c4c8eba80",
+   measurementId: "G-RCSREMC1E7"
+};
